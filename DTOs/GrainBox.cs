d8cc9249aeb6dfffb113cc17ef8bca307fd07ef9
Rentoki <146650376+SundenJaeger@users.noreply.github.com>
2026-08-04 14:29:35 +0800

    Add GrainBox dto

---
+++ DTOs/GrainBox.cs
@@ -0,0 +1,11 @@
+﻿namespace ImmatureBackend.DTOs;
+
+public sealed record GrainBox
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+    public int Width { get; init; }
+    public int Height { get; init; }
+    public double? Confidence { get; init; }
+    public string? Action { get; init; }
+}

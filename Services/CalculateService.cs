57ee24cf1230638dfbf2150f04ac07057a3164a7
Rentoki <146650376+SundenJaeger@users.noreply.github.com>
2026-08-04 14:33:04 +0800

    Add CalculateService

---
+++ Services/CalculateService.cs
@@ -0,0 +1,28 @@
+﻿using ImmatureBackend.Services.Interfaces;
+
+namespace ImmatureBackend.Services;
+
+public class CalculateService : ICalculationService
+{
+    public decimal CalculatePercentage(decimal weight)
+    {
+        if (weight <= 0)
+        {
+            return 0;
+        }
+
+        return Math.Round(weight / 30m * 100m, 2);
+    }
+
+    public string AssignGrade(decimal percentage)
+    {
+        return percentage switch
+        {
+            < 2.0m => "Pr",
+            <= 5.0m => "G1",
+            <= 10.0m => "G2",
+            <= 15.0m => "G3",
+            _ => "Below Standard"
+        };
+    }
+}
---
+++ Services/Interfaces/ICalculationService.cs
@@ -0,0 +1,7 @@
+﻿namespace ImmatureBackend.Services.Interfaces;
+
+public interface ICalculationService
+{
+    decimal CalculatePercentage(decimal weight);
+    string AssignGrade(decimal percentage);
+}

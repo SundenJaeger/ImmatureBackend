42bbfb7ebe70dfa4b58a73837ca07a867b11266b
Rentoki <146650376+SundenJaeger@users.noreply.github.com>
2026-08-31 19:45:33 +0800

    Add validator for PredictRequest

--- ImmatureBackend.Application/Requests/PredictRequest.cs
+++ ImmatureBackend.Application/Requests/PredictRequest.cs
@@ -1,11 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace ImmatureBackend.Application.Requests;
 
 public sealed record PredictRequest
 {
-    [Required] public IFormFile Image { get; init; } = null!;
-    [Required] public string TechnicianName { get; init; } = string.Empty;
-    [Required] public string SampleId { get; init; } = string.Empty;
+    public IFormFile Image { get; init; } = null!;
+    public string TechnicianName { get; init; } = string.Empty;
+    public string SampleId { get; init; } = string.Empty;
 }
---
+++ ImmatureBackend.Application/Validators/PredictRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using ImmatureBackend.Application.Requests;
+
+namespace ImmatureBackend.Application.Validators;
+
+public class PredictRequestValidator : AbstractValidator<PredictRequest>
+{
+    public PredictRequestValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(request => request.Image)
+            .NotNull().WithMessage("Image is required.");
+
+        RuleFor(request => request.TechnicianName)
+            .NotEmpty().WithMessage("Technician Name is required.");
+
+        RuleFor(request => request.SampleId)
+            .NotEmpty().WithMessage("Sample ID is required.");
+    }
+}

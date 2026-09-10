7db9aec9c9901524d4c99dae1dbf18b30dd12675
Rentoki <146650376+SundenJaeger@users.noreply.github.com>
2026-09-10 15:24:23 +0800

    Remove Supabase attributes from ReplicateEntity

--- ImmatureBackend.Domain/Models/ReplicateEntity.cs
+++ ImmatureBackend.Domain/Models/ReplicateEntity.cs
@@ -1,35 +1,24 @@
 ﻿using ImmatureBackend.Domain.Enums;
 using ImmatureBackend.Domain.Utils;
 using Newtonsoft.Json;
-using Supabase.Postgrest.Attributes;
-using Supabase.Postgrest.Models;
 
 namespace ImmatureBackend.Domain.Models;
 
-[Table("replicates")]
-public class ReplicateEntity : BaseModel
+public class ReplicateEntity
 {
-    [PrimaryKey("id", false)] public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; }
+    public required string TechnicianName { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public required string SampleId { get; set; }
+    public required string AiPredictedGrains { get; set; }
+    public required string ConfirmedGrains { get; set; }
+    public decimal ImmatureWeight { get; set; }
+    public decimal Percentage { get; set; }
 
-    [Column("technician_name")] public string TechnicianName { get; set; } = string.Empty;
+    public required string Grade { get; set; }
 
-    [Column("created_at")] public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-    [Column("sample_id")] public string SampleId { get; set; } = string.Empty;
-
-    [Column("ai_predicted_grains")] public string AiPredictedGrains { get; set; } = string.Empty;
-
-    [Column("confirmed_grains")] public string ConfirmedGrains { get; set; } = string.Empty;
-
-    [Column("immature_weight")] public decimal ImmatureWeight { get; set; }
-
-    [Column("percentage")] public decimal Percentage { get; set; }
-
-    [Column("grade")] public string Grade { get; set; } = string.Empty;
-
-    [Column("original_image")]
     [JsonConverter(typeof(ByteaConverter))]
     public byte[]? OriginalImage { get; set; }
 
-    [Column("review_status")] public ReviewStatus ReviewStatus { get; set; } = ReviewStatus.Review;
+    public ReviewStatus ReviewStatus { get; set; }
 }

7b0dd4339364489b0b4342d3c3125f97075816a6
Rentoki <146650376+SundenJaeger@users.noreply.github.com>
2026-09-10 22:58:57 +0800

    Add image size and empty file validation in PredictRequest and ReplicateRequest validators

--- ImmatureBackend.Application/Validators/ReplicateRequestValidator.cs
+++ ImmatureBackend.Application/Validators/ReplicateRequestValidator.cs
@@ -10,7 +10,9 @@ public class ReplicateRequestValidator : AbstractValidator<ReplicateRequest>
         RuleLevelCascadeMode = CascadeMode.Stop;
 
         RuleFor(request => request.Image)
-            .NotNull().WithMessage("Image is required.");
+            .NotNull().WithMessage("Image is required.")
+            .Must(image => image!.Length > 0).WithMessage("Image cannot be empty.")
+            .Must(image => image!.Length <= 10 * 1024 * 1024).WithMessage("Image cannot be larger than 10 MB.");
 
         RuleFor(request => request.TechnicianName)
             .NotEmpty().WithMessage("Technician Name is required.");
